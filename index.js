6d9a59a8e13502ac3d3449e0390947c0d08f7097
rneto <rrnoak@gmail.com>
2020-05-08 08:38:56 +0200

    feat: Change index content.

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 // Start with the index.html and start editing.
 
 import './style.css';
+
+const appTitle = document.getElementById('appTitle');
+appTitle.title.innerHTML = `<h1>CodesRank Front-end developer practices</h1>`;
